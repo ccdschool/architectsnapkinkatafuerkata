c9f5e7253f2a18d6336c6ddae598643b71fa777b
Ralf Westphal <info@ralfw.de>
2014-05-05 17:32:06 +0200

    Gerüsttests gelöscht

--- CSV tabellieren/csv/csv.tabellieren.tests/gerüste.cs
+++ CSV tabellieren/csv/csv.tabellieren.tests/gerüste.cs
@@ -7,19 +7,5 @@ namespace csv.tabellieren.tests
     [TestFixture]
     public class gerüste
     {
-        [Test]
-        public void Spaltenbreiten_ermitteln()
-        {
-            var mi = typeof (CsvTabellierer).GetMethod("Spaltenbreiten_ermitteln", BindingFlags.NonPublic | BindingFlags.Instance);
-            var sut = new CsvTabellierer();
-
-            var tabellenteile = new Tuple<string[], string[][]>(
-                new[]{"1", "22", "333"},
-                new[]{new[]{"a", "b", "cccc"}});
-
-            var result = mi.Invoke(sut, new object[]{ tabellenteile });
-
-            Assert.AreEqual(new[]{1, 2, 4}, result);
-        } 
     }
 }
