0ca31709f81166bcb9738df6d377172aaf2a6492
Ralf Westphal <info@ralfw.de>
2014-05-05 17:58:09 +0200

    Refaktorisierung: Tabellierer-Klasse herausgezogen

--- CSV tabellieren/csv/csv.tabellieren/CsvTabellierer.cs
+++ CSV tabellieren/csv/csv.tabellieren/CsvTabellierer.cs
@@ -10,55 +10,11 @@ namespace csv.tabellieren
         public IEnumerable<string> Tabellieren(IEnumerable<string> csvText)
         {
             var parser = new CsvParser();
+            var tabellierer = new Tabellierer();
 
             var records = parser.Parsen(csvText);
             var tabellenteile = parser.Header_von_Body_trennen(records);
-            return Tabelle_bauen(tabellenteile);
-        }
-
-
-        private IEnumerable<string> Tabelle_bauen(Tuple<string[], string[][]> tabellenteile)
-        {
-            var spaltenbreiten = Spaltenbreiten_ermitteln(tabellenteile);
-            var tabellierter_header = Header_formatieren(tabellenteile.Item1, spaltenbreiten);
-            var tabellierter_body = Body_formatieren(tabellenteile.Item2, spaltenbreiten);
-            return Tabelle_zusammenbauen(tabellierter_header, tabellierter_body);
-        }
-
-
-        private int[] Spaltenbreiten_ermitteln(Tuple<string[], string[][]> tabellenteile)
-        {
-            var spaltenbreiten = tabellenteile.Item1.Select(v => v.Length).ToArray();
-            for (var i = 0; i < spaltenbreiten.Length; i++)
-                spaltenbreiten[i] = Math.Max(spaltenbreiten[i], 
-                                             tabellenteile.Item2.Select(r => r[i]).Max(v => v.Length));
-            return spaltenbreiten;
-        }
-
-
-        private IEnumerable<string> Header_formatieren(string[] headerRecord, int[] spaltenbreiten)
-        {
-            yield return Body_formatieren(new[] {headerRecord}, spaltenbreiten).First();
-
-            var unterstreichungen = spaltenbreiten.Select(breite => "".PadRight(breite, '-'));
-            yield return string.Join("+", unterstreichungen);
-        }
-
-
-        private IEnumerable<string> Body_formatieren(IEnumerable<string[]> bodyRecords, int[] spaltenbreiten)
-        {
-            return bodyRecords.Select(br => string.Join("|", Werte_auf_Spaltenbreiten_bringen(br, spaltenbreiten)));
-        }
-
-        private string[] Werte_auf_Spaltenbreiten_bringen(IEnumerable<string> werte, int[] spaltenbreiten)
-        {
-            return werte.Select((w, i) => w.PadRight(spaltenbreiten[i], ' ')).ToArray();
-        }
-
-
-        private IEnumerable<string> Tabelle_zusammenbauen(IEnumerable<string> tabellierterHeader, IEnumerable<string> tabellierterBody)
-        {
-            return tabellierterHeader.Concat(tabellierterBody);
+            return tabellierer.Tabelle_bauen(tabellenteile);
         }
     }
 }
---
+++ CSV tabellieren/csv/csv.tabellieren/Tabellierer.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace csv.tabellieren
+{
+    public class Tabellierer
+    {
+        public IEnumerable<string> Tabelle_bauen(Tuple<string[], string[][]> tabellenteile)
+        {
+            var spaltenbreiten = Spaltenbreiten_ermitteln(tabellenteile);
+            var tabellierter_header = Header_formatieren(tabellenteile.Item1, spaltenbreiten);
+            var tabellierter_body = Body_formatieren(tabellenteile.Item2, spaltenbreiten);
+            return Tabelle_zusammenbauen(tabellierter_header, tabellierter_body);
+        }
+
+        private int[] Spaltenbreiten_ermitteln(Tuple<string[], string[][]> tabellenteile)
+        {
+            var spaltenbreiten = tabellenteile.Item1.Select(v => v.Length).ToArray();
+            for (var i = 0; i < spaltenbreiten.Length; i++)
+                spaltenbreiten[i] = Math.Max(spaltenbreiten[i], 
+                                             tabellenteile.Item2.Select(r => r[i]).Max(v => v.Length));
+            return spaltenbreiten;
+        }
+
+        private IEnumerable<string> Header_formatieren(string[] headerRecord, int[] spaltenbreiten)
+        {
+            yield return Enumerable.First<string>(Body_formatieren(new[] {headerRecord}, spaltenbreiten));
+
+            var unterstreichungen = spaltenbreiten.Select(breite => "".PadRight(breite, '-'));
+            yield return string.Join("+", unterstreichungen);
+        }
+
+        private IEnumerable<string> Body_formatieren(IEnumerable<string[]> bodyRecords, int[] spaltenbreiten)
+        {
+            return bodyRecords.Select<string[], string>(br => string.Join("|", Werte_auf_Spaltenbreiten_bringen(br, spaltenbreiten)));
+        }
+
+        private string[] Werte_auf_Spaltenbreiten_bringen(IEnumerable<string> werte, int[] spaltenbreiten)
+        {
+            return werte.Select((w, i) => w.PadRight(spaltenbreiten[i], ' ')).ToArray();
+        }
+
+        private IEnumerable<string> Tabelle_zusammenbauen(IEnumerable<string> tabellierterHeader, IEnumerable<string> tabellierterBody)
+        {
+            return tabellierterHeader.Concat(tabellierterBody);
+        }
+    }
+}
