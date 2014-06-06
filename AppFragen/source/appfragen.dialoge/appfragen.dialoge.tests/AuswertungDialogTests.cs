34311d9b313902102364a1c7e3ba4592a226e493
Stefan Lieser <stefan@lieser-online.de>
2014-06-06 12:28:53 +0200

    AuswertungDialog ergänzt

---
+++ AppFragen/source/appfragen.dialoge/appfragen.dialoge.tests/AuswertungDialogTests.cs
@@ -0,0 +1,15 @@
+﻿using appfragen.contracts;
+using NUnit.Framework;
+
+namespace appfragen.dialoge.tests
+{
+    [TestFixture]
+    public class AuswertungDialogTests
+    {
+        [Test, Explicit, RequiresSTA]
+        public void Anzeigen() {
+            var sut = new AuswertungDialog();
+            sut.Auswertung_Anzeigen(new Auswertung());
+        } 
+    }
+}
---
+++ AppFragen/source/appfragen.dialoge/appfragen.dialoge/AuswertungDialog.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+using appfragen.contracts;
+
+namespace appfragen.dialoge
+{
+    public partial class AuswertungDialog : Window
+    {
+        public AuswertungDialog() {
+            InitializeComponent();
+        }
+
+        public void Auswertung_Anzeigen(Auswertung auswertung) {
+            ShowDialog();
+        }
+    }
+}
